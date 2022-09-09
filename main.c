7b686b9d7654e2254042251f9650a99466b8f19c
Luzefiru <ian.deejay.iv@gmail.com>
2022-09-09 16:59:49 +0800

    Added documentation & main function boilerplate

---
+++ main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ * @author Christian de Jesus
+ * @brief Gets an amount in USD from the user & outputs the converted denominations in PHP.
+ * @version 0.1
+ * @date 2022-09-09
+ * 
+ * @copyright Copyright (c) 2022
+ */
+
+#include <stdio.h>
+
+#define CONVERSION_RATE_USD_TO_PHP 49.2
+
+int main(void)
+{
+    
+
+    return 0;
+}
